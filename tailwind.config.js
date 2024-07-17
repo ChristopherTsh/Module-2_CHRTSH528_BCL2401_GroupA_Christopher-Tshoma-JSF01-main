fff7e0f5193071c563fec6371ea8f68a9b11ec22
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-07-17 11:42:10 +0200

    created vite set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [ "./index.html", // don't forget to add other files and directories
+    "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
