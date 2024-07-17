480d28d71d4e598c8729606ddd85280957f9d45b
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-07-17 15:26:11 +0200

    "Removed counter.js file, updated index.html with new card section and Tailwind CSS, and modified main.js to initialize Alpine data and fetch products"

---
+++ Api.js
@@ -0,0 +1,16 @@
+export async function fetchProducts(){
+      try{
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        return data.map(product => ({
+          image: product.image,
+          id: product.id,
+          title: product.title,
+          price: product.price,
+          rating: product.rating
+        }));
+      } catch (error) {
+        console.log('Error fetching products', error)
+        throw error;
+      }
+    }
--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 import './style.css'
 import Alpine from 'alpinejs'
+import { fetchProducts } from './Api.js';
+
 window.Alpine = Alpine
+
+document.addEventListener('alpine:init', () => {
+  Alpine.data('productApp', () => ({
+    products: [],
+    loading: true,
+
+    async init() {
+        this.loading = true;
+        try {
+            this.products = await fetchProducts();
+        } catch (error) {
+            console.error('Failed to fetch products', error);
+        } finally {
+            this.loading = false;
+        }
+    }
+}));
+
+}
+)
 Alpine.start()
